acd4b07dafdd322c09021729c1e7e3f8593dbe93
Ahmed Hany <ah7608867@gmail.com>
2024-12-29 12:36:49 +0200

    piplining

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -23,21 +23,40 @@ const run = async () => {
 		solution: JSON.stringify({ name: 'John', age: 30 })
 	 */
 
-/*	await client.hSet('car', {
-		make: 'Toyota',
-		model: 'Corolla',
-		year: 2019,
-		color: 'red',
-		price: 20000,
-		owner: JSON.stringify({ name: 'John', age: 30 })
-	});*/
+	/*	await client.hSet('car', {
+			make: 'Toyota',
+			model: 'Corolla',
+			year: 2019,
+			color: 'red',
+			price: 20000,
+			owner: JSON.stringify({ name: 'John', age: 30 })
+		});*/
 
 	// undefined and null values of get method
 	const car = await client.hGetAll('car5');
-	if(Object.keys(car).length === 0) {
+	if (Object.keys(car).length === 0) {
 		console.log('Car not found in Redis');
 		return;
 	}
 	console.log(car);
 };
-run();
+//run();
+
+
+
+async function workWithPiplines() {
+
+	await client.hSet('car', { make: 'Toyota', model: 'Corolla', year: 2019 });
+	await client.hSet('car2', { make: 'Toyota2', model: 'Corolla2', year: 2019 });
+	await client.hSet('car3', { make: 'Toyota3', model: 'Corolla3', year: 2019 });
+
+	const result = await Promise.all([
+		client.hGetAll('car'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+
+	console.log(result);
+}
+
+workWithPiplines();
