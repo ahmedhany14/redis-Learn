9f99d98d167bd88a724a5449599d289d4fa9d7f9
Ahmed Hany <ah7608867@gmail.com>
2025-01-08 23:24:52 +0200

    get items according to their views and usage if sort

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,25 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsKeyByView, itemKeys } from '$services/keys';
+import { getItems } from '$services/queries/items/items';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	/*const ids = await client.zRange(itemsKeyByView(), 0, '+inf', {
+		BY: 'SCORE',
+		LIMIT: {
+			offset,
+			count
+		}
+	});*/
+
+	const ids = (await client.sort(itemsKeyByView(), {
+		GET: ['#'],
+		BY: `${itemKeys('*')}->views`,
+		DIRECTION: order, // DESC or ASC
+		LIMIT: {
+			offset: offset,
+			count: count
+		}
+		//ALPHA: true // if ids are strings, you want to sort them alphabetically
+	})) as string[];
+	return await getItems(ids);
+};
