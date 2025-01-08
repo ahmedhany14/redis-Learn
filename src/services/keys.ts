630a908cc3a15e69bd77b084093c4f750dc03c92
Ahmed Hany <ah7608867@gmail.com>
2025-01-08 18:07:11 +0200

    implement getting user by his name with sorted sets

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -4,3 +4,4 @@ export const sessionKeys = (id: string) => `session:${id}`;
 export const itemKeys = (id: string) => `items:${id}`;
 export const userNameKey = () => `username:unique`;
 export const userLikedItems = (id: string) => `user:${id}:likes`;
+export const usernameKey = () => `username`;
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,39 +1,50 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { userKeys, userNameKey } from '$services/keys';
+import { userKeys, userNameKey, usernameKey } from '$services/keys';
 
+export const getUserByUsername = async (username: string) => {
+	let id = (await client.zScore(usernameKey(), username))?.toString(16);
+	const user = await client.hGetAll(userKeys(id));
 
-export const getUserByUsername = async (username: string) => { };
+	if (Object.keys(user).length === 0) return null;
+
+	return deserialization(user, id);
+};
 
 export const getUserById = async (id: string) => {
-    const user = await client.hGetAll(userKeys(id)); // will return an object with the user's data
-    return deserialization(user, id);
+	const user = await client.hGetAll(userKeys(id)); // will return an object with the user's data
+	return deserialization(user, id);
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-    const id = genId();
-    const is_member = await client.sIsMember(userNameKey(), attrs.username);
+	const id = genId();
+	const is_member = await client.sIsMember(userNameKey(), attrs.username);
 
-    if(is_member) throw new Error('Username already exists');
-    await client.sAdd(userNameKey(), attrs.username);
-    await client.hSet(userKeys(id), serialization(attrs));
+	if (is_member) throw new Error('Username already exists');
+	await client.sAdd(userNameKey(), attrs.username);
+	await client.hSet(userKeys(id), serialization(attrs));
 
-    return id;
+	// create a username sorted set to store the user's name and id
+	await client.zAdd(usernameKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
+
+	return id;
 };
 
 function serialization(user: CreateUserAttrs) {
-    return {
-        username: user.username,
-        password: user.password,
-    }
+	return {
+		username: user.username,
+		password: user.password
+	};
 }
 
 function deserialization(user: any, id: string) {
-    return {
-        id,
-        username: user.username,
-        password: user.password,
-    }
-
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
 }
