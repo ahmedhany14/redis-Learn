d69bfa4c649467a99cd632f2f6814fefefb5bd5d
Ahmed Hany <ah7608867@gmail.com>
2025-02-04 02:54:04 +0200

    locks and solving watch problem and (aborted and re-executed) problem

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,5 +1,5 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { bidHistoryKeys, itemKeys, itemsKeyByPrice } from '$services/keys';
 import { DateTime } from 'luxon';
 import { getItem } from '$services/queries/items';
@@ -19,58 +19,112 @@ transaction steps:
  set the new data
  execute the transaction
  */
-export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient) => {
-		// watch the item key
-		await isolatedClient.watch(itemKeys(attrs.itemId));
 
+/*
+
+LOCKS
+
+As you know, we solve concurrency issues using transactions, but what if we have a lot of transactions at the same time.
+This will lead to a lot of transactions being aborted and re-executed, which is not efficient.
+This will lead us to miss many bids, and the user will not be able to bid on the item.
+
+So we will use locks to solve this issue.
+Which means that we will lock the item key when we start the transaction, and we will unlock it when we finish the transaction.
+
+But how locks work in Redis?
+1. you declare the lock key, the number of retries and the timeout that the lock will be released after.
+2. you try to acquire the lock, if you can't acquire it, you will wait for the timeout and try again.
+3. you will start the transaction and execute the commands.
+4. you will release the lock.
+
+ */
+
+export const createBid = async (attrs: CreateBidAttrs) => {
+	// implementation of the lock
+	return withLock(attrs.itemId, async () => {
+		// first, we will fetch the item to check if it exists
 		const item = await getItem(attrs.itemId);
 
+		// do some validations
 		if (!item) throw new Error('Item not found');
 
+		console.log(item.price, attrs.amount);
 		if (item.price >= attrs.amount)
 			throw new Error('Bid amount should be greater than the current price');
 
 		if (item.endingAt.diff(DateTime.now()).toMillis() < 0)
 			throw new Error('Item has already ended');
-
-		client.multi();
 		// update the item with the new bid
-
 		const key = bidHistoryKeys(attrs.itemId);
-
 		const value = seralizeBid(attrs);
 
-		// await Promise.all([
-		// 	client.rPush(key, value),
-		// 	client.hSet(itemKeys(item.id), {
-		// 		highestBidUserId: attrs.userId,
-		// 		price: attrs.amount,
-		// 		bids: item.bids + 1
-		// 	})
-		// ]);
-
-		// start the transaction and execute the commands
-		return isolatedClient
-			.multi()
-			.rPush(key, value)
-			.hSet(itemKeys(item.id), {
+		// create the bid if everything is okay
+		return Promise.all([
+			client.rPush(key, value),
+			client.hSet(itemKeys(item.id), {
 				highestBidUserId: attrs.userId,
 				price: attrs.amount,
 				bids: item.bids + 1
-			})
-			.zAdd(itemsKeyByPrice() , {
+			}),
+			client.zAdd(itemsKeyByPrice(), {
 				value: item.id,
 				score: attrs.amount
 			})
-			.exec();
+		]);
 
-		// write this script in the browser console to test the concurrency issue
-
-		// for(let i = 0; i < 100; i++) {
-		// 	$0click();
-		// }
 	});
+
+	/*	return client.executeIsolated(async (isolatedClient) => {
+			// watch the item key
+			await isolatedClient.watch(itemKeys(attrs.itemId));
+	
+			const item = await getItem(attrs.itemId);
+	
+			if (!item) throw new Error('Item not found');
+	
+			if (item.price >= attrs.amount)
+				throw new Error('Bid amount should be greater than the current price');
+	
+			if (item.endingAt.diff(DateTime.now()).toMillis() < 0)
+				throw new Error('Item has already ended');
+	
+			client.multi();
+			// update the item with the new bid
+	
+			const key = bidHistoryKeys(attrs.itemId);
+	
+			const value = seralizeBid(attrs);
+	
+			// await Promise.all([
+			// 	client.rPush(key, value),
+			// 	client.hSet(itemKeys(item.id), {
+			// 		highestBidUserId: attrs.userId,
+			// 		price: attrs.amount,
+			// 		bids: item.bids + 1
+			// 	})
+			// ]);
+	
+			// start the transaction and execute the commands
+			return isolatedClient
+				.multi()
+				.rPush(key, value)
+				.hSet(itemKeys(item.id), {
+					highestBidUserId: attrs.userId,
+					price: attrs.amount,
+					bids: item.bids + 1
+				})
+				.zAdd(itemsKeyByPrice() , {
+					value: item.id,
+					score: attrs.amount
+				})
+				.exec();
+	
+			// write this script in the browser console to test the concurrency issue
+	
+			// for(let i = 0; i < 100; i++) {
+			// 	$0click();
+			// }
+		});*/
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,33 @@
-export const withLock = async () => {};
+import { client } from '../../../worker/client';
+
+export const withLock = async (key: string, cd: () => any) => {
+	// implementation of the lock
+
+	//1. initialize timeout and retry count
+	const timeout = 50, retry = 10; // will wait for 100ms
+	const total_timeout = timeout * retry;
+	const token = 'used'; // generate a random token to identify the lock
+	const lock_key = `lock#${key}`;
+
+
+	for (let i = 0; i < retry; i++) {
+		//2. you try to acquire the lock, if you can't acquire it, you will wait for the timeout and try again.
+		const acquired = await client.set(lock_key, token, {
+			NX: true, // set the lock if it doesn't exist, return false if it exists
+			PX: total_timeout, // set the expiration time in milliseconds
+			//EX: total_timeout // set the expiration time in seconds
+			/*
+			Note: we should use one of them (PX or EX) to set the expiration time
+			 */
+		});
+		if (acquired) {
+			await client.del(lock_key);
+			return await cd();
+		}
+
+		await pause(timeout);
+	}
+};
 
 const buildClientProxy = () => {};
 
